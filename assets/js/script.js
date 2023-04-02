4cc381c379576df9cf2129fb62e9a0de789062cc
Amman Nega <ammannega@gmail.com>
2023-04-02 16:56:35 -0700

    wrote psuedocode for game functionality

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//TODO: Game starts as soon as I hit the start button
+    //TODO: timer starts counting down
+    //TODO: Question appears on the screen
+        //TODO: Four corresponding answers are presented
+
+//TODO: When I click one of four answers
+    //TODO: If answer is incorrect, subtract 10 seconds from timer
+    //TODO: Regardless of correct answer, move to next question
+
+//TODO: Display if previous answer was right or wrong
+
+//TODO: If all questions are answered or timer reaches 0, transition to game over screen
+    //TODO: display final score as amount of time remaining
+
